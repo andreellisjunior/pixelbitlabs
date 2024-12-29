dfce9f9ee8e12d8805b19991992b1b081a5d9360
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2024-12-29 08:10:43 -0600

    correcting contact link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ export const navigation = [
   { name: 'Services', href: '#services' },
   { name: 'About', href: '#about' },
   { name: 'Portfolio', href: '#portfolio' },
-  { name: 'Contact', href: 'support@pixelbitlabs.com' },
+  { name: 'Contact', href: 'mailto:support@pixelbitlabs.com' },
 ];
